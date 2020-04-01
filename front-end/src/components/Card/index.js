3ab67f7673992c1149ad6977eead10f9497de56c
unknown <rafaelbatista.sth@gmail.com>
2020-04-01 19:29:38 -0300

    Finalizado o layout dos componentes.

--- front-end/src/components/Card/index.js
+++ front-end/src/components/Card/index.js
@@ -2,17 +2,16 @@ import React from "react";
 
 import { Container, Label } from "./styles";
 
-export default function Card() {
+export default function Card({ data }) {
   return (
     <Container>
       <header>
-        <Label color="#7159C1" />
+        {data.labels.map(label => (
+          <Label key={label} color={label} />
+        ))}
       </header>
-      <p>Fazer a migração completa de servidor</p>
-      <img
-        src="https://avatars3.githubusercontent.com/u/59888617?s=120&v=4"
-        alt=""
-      />
+      <p>{data.content}</p>
+      {data.user && <img src={data.user} alt="" />}
     </Container>
   );
 }
